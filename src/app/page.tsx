1123422fc58a42f743ba54e090b236e50cafd978
jgandrade <johnglenn.andrade@gmail.com>
2024-01-31 09:44:56 +0800

    [master] Removed Testing in the project

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,14 @@
 import { Box, Typography } from "@mui/material";
-// import Popular from "@/components/Popular/Popular";
-// import RecentEpisodes from "@/components/RecentEpisodes/RecentEpisodes";
-// import Trending from "@/components/Trending/Trending";
+import Popular from "@/components/Popular/Popular";
+import Trending from "@/components/Trending/Trending";
 
 const Home = async () => {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <Typography variant="h4">Aniweb</Typography>
       <Box>
-        {/* <Popular />
-        <RecentEpisodes />
-        <Trending /> */}
+        <Popular />
+        <Trending />
       </Box>
     </main>
   );
