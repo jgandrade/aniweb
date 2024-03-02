1eca81a95dd965858d398ea8b958b17e3dceb16b
jgandrade <johnglenn.andrade@gmail.com>
2024-03-03 07:45:33 +0800

    [watch] added error boundary and created useWatch

---
+++ src/app/error.tsx
@@ -0,0 +1,6 @@
+"use client";
+const Error = () => {
+  return <div>Error has occured in the server</div>;
+};
+
+export default Error;
---
+++ src/app/watch/Watch.tsx
@@ -0,0 +1,41 @@
+"use client";
+
+import Error from "@/components/Error/Error";
+import VideoPlayer from "@/components/VideoPlayer/VideoPlayer";
+import useWatch from "@/hooks/useWatch";
+import { AnimeInfoType } from "@/types/AnimeTypes";
+import { Loader2 } from "lucide-react";
+import { useSearchParams } from "next/navigation";
+
+const Watch = ({ animeInfo }: { animeInfo: AnimeInfoType }) => {
+  const searchParams = useSearchParams();
+  const episode = searchParams.get("episode");
+  const { data, isLoading } = useWatch(
+    animeInfo.episodes[Number(episode) - 1].id
+  );
+
+  if (isLoading) {
+    return <Loader2 />;
+  }
+
+  if (data) {
+    return (
+      <div>
+        {!isLoading && (
+          <VideoPlayer
+            videoUrl={
+              data.sources[5]?.url ||
+              data.sources[4]?.url ||
+              data.sources[3]?.url ||
+              data.sources[2]?.url ||
+              data.sources[1]?.url ||
+              data.sources[0]?.url
+            }
+          />
+        )}
+      </div>
+    );
+  }
+};
+
+export default Watch;
